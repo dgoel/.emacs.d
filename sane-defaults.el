3506fae6f87056e16ba1db899c9967c87e296467
Dhiraj Goel <dhiraj.goel@gmail.com>
2013-02-10 17:23:17 -0800

    Create backup files in system tmp folder.

--- sane-defaults.el
+++ sane-defaults.el
@@ -14,6 +14,22 @@
 ;; Move files to trash when deleting
 (setq delete-by-moving-to-trash t)
 
+(setq
+ backup-by-copying t ; don't clobber symlinks
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t) ; use versioned backups
+
+;; Write backup files to own directory
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
+
 ;; Real emacs knights don't use shift to mark things
 (setq shift-select-mode nil)
 
