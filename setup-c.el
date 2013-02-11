f86d7156a84e5f760b20d20112a9f2c88f4c758a
Dhiraj Goel <dhiraj.goel@gmail.com>
2013-02-10 17:21:01 -0800

    Allow to switch between source and header file.

--- setup-c.el
+++ setup-c.el
@@ -4,6 +4,10 @@
 ; (setq c-set-style "linux" c-basic-offset 4)
 (setq c-default-style "linux" c-basic-offset 4)
 
+;;; switch between source and header file in C-mode
+(add-hook 'c-mode-common-hook
+          (lambda()
+            (local-set-key (kbd "C-c o") 'ff-find-other-file)))
 
 ;;; Show the current function
 ; (add-hook 'c-mode-common-hook (lambda () (which-function-mode t)))
