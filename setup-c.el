75a504dfe19b324a4a363797cd3bd15f34cbf700
Dhiraj Goel <dhiraj.goel@gmail.com>
2013-04-15 23:28:25 -0700

    Fixed setting up default C formatting

--- setup-c.el
+++ setup-c.el
@@ -1,26 +1,20 @@
 ;;(setq-default show-trailing-whitespace t)
-;(setq-default indent-tabs-mode nil) ; always replace tabs with spaces
-;(setq-default tab-width 4) ; set tab width to 4 for all buffers
-; (setq c-set-style "linux" c-basic-offset 4)
-;(setq c-default-style "linux" c-basic-offset 4)
 
-(defun my-cc-style()
-  (c-set-style "linux")
-  ; setup offset before using them
-  (setq c-basic-offset 4)
-  (setq tab-width 4)
-  ; now setup dependent offsets
-  (c-set-offset 'innamespace '0) ; dont indent for -  namespace foo {
-  (c-set-offset 'inextern-lang '0) ; dont indent for - extern 'c' {
-  (c-set-offset 'inline-open '0); indentation for inclass methods
-  (c-set-offset 'label '*)
-;  (c-set-offset 'case-label '*)
-;  (c-set-offset 'access-label '0)
-  (setq indent-tabs-mode nil)
-  )
+(c-add-style "mine" '("linux"
+                      (c-basic-offset . 4)
+                      (indent-tabs-mode . nil)
+                      (tab-width . 4)
+                      (c-offsets-alist
+                       (innamespace . 0)   ; dont indent for namespace foo {
+                       (inextern-lang . 0) ; dont indent for extern C
+                       (inline-open . 0)  ; indentation for inclass methods
+                       )
+                      ))
 
-(add-hook 'c++-mode-hook 'my-cc-style)
-(add-hook 'c-common-mode-hook 'my-cc-style)
+(setq c-default-style "mine")
+;(c-set-offset 'label '*)
+;(c-set-offset 'case-label '*)
+;(c-set-offset 'access-label '0)
 
 ;;; switch between source and header file in C-mode
 (add-hook 'c-mode-common-hook
@@ -31,3 +25,4 @@
 ; (add-hook 'c-mode-common-hook (lambda () (which-function-mode t)))
 
 (provide 'setup-c)
+
