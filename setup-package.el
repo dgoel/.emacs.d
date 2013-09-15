a19cd4a0b166c21f02fa6e614843954170528c28
Dhiraj Goel <dhiraj.goel@gmail.com>
2013-09-14 23:27:03 -0700

    Allow package installation from marmalade

--- setup-package.el
+++ setup-package.el
@@ -3,6 +3,7 @@
 
 ;; Add melpa to package repos
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (package-initialize)
 
