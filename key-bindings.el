f1ace1062648145fa27febd9764c46e1276a1f02
Dhiraj Goel <dhiraj.goel@gmail.com>
2013-10-20 14:56:36 -0700

    Remove commented out line

--- key-bindings.el
+++ key-bindings.el
@@ -152,7 +152,6 @@
 (windmove-default-keybindings) ;; Shift+direction
 (global-set-key (kbd "C-x -") 'rotate-windows)
 (global-set-key (kbd "C-x |") 'toggle-window-split)
-; (global-unset-key (kbd "C-x C-+")) ;; don't zoom like this
 
 (global-set-key (kbd "C-x 3") 'split-window-right-and-move-there-dammit)
 
