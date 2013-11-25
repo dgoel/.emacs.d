9c6eba26eabfc78f2c54f76c937e2677c5ae2964
Dhiraj Goel <dhiraj.goel@gmail.com>
2013-11-24 18:51:09 -0800

    Use projectile and enable for c common mode

--- setup-c.el
+++ setup-c.el
@@ -16,6 +16,31 @@
 ;(c-set-offset 'case-label '*)
 ;(c-set-offset 'access-label '0)
 
+;; projectile mode
+(require 'projectile)
+(setq projectile-enable-caching t) ;; enable caching
+;; @see https://gist.github.com/rejeep/5933343
+(defun rejeep-projectile-completion-fn (prompt choises)
+  "Projectile completion function that only shows file name.
+If two files have same name, new completion appears to select between
+them. These include the path relative to the project root."
+  (interactive)
+  (let* ((stripped-choises
+          (-uniq (--map (file-name-nondirectory it) choises)))
+         (choise
+          (ido-completing-read prompt stripped-choises))
+         (matching-files
+          (-filter
+           (lambda (file)
+             (equal (file-name-nondirectory file) choise))
+           choises)))
+    (if (> (length matching-files) 1)
+        (ido-completing-read prompt matching-files)
+      (car matching-files))))
+(setq projectile-completion-system 'rejeep-projectile-completion-fn)
+(add-hook 'c-mode-common-hook (lambda () (projectile-mode 1)))
+
+
 ;;; switch between source and header file in C-mode
 (add-hook 'c-mode-common-hook
           (lambda()
