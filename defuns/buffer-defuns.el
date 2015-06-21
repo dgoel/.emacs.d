b7b309b760f8546ad70a21ce4f2d84918374498d
Dhiraj Goel <dhiraj.goel@gmail.com>
2015-06-21 15:24:43 -0700

    Update config for recentf

--- defuns/buffer-defuns.el
+++ defuns/buffer-defuns.el
@@ -159,17 +159,6 @@ Including indent-buffer, which should not be called automatically on save."
 (defun shorter-file-name (file-name)
   (s-chop-prefix user-home-directory file-name))
 
-(defun recentf--file-cons (file-name)
-  (cons (shorter-file-name file-name) file-name))
-
-(defun recentf-ido-find-file ()
-  "Find a recent file using ido."
-  (interactive)
-  (let* ((recent-files (mapcar 'recentf--file-cons recentf-list))
-         (files (mapcar 'car recent-files))
-         (file (completing-read "Choose recent file: " files)))
-    (find-file (cdr (assoc file recent-files)))))
-
 (defun kill-other-buffers ()
   "Kill all other buffers."
   (interactive)
