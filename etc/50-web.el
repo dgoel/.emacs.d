416b161cccdaa20059f86e9ad1d7ddc8697d2f6f
Dhiraj Goel <dhiraj.goel@gmail.com>
2015-06-21 22:24:12 -0700

    50-web: congfigure browse-url and eww

--- etc/50-web.el
+++ etc/50-web.el
@@ -2,5 +2,24 @@
 (use-package elfeed
   :defer t
   :commands (elfeed elfeed-update)
-  :config (require 'feeds "personal/feeds.el")
-  )
+  :config (require 'feeds "personal/feeds.el"))
+
+(setq url-cookie-file (expand-file-name "url" var-dir))
+
+(use-package browse-url
+  :defer t
+  :commands (browse-url browse-url-at-point browse-url-at-mouse)
+  :config
+  (use-package eww)
+  (defun choose-browser (url &rest args)
+    (interactive "sURL: ")
+    (if (y-or-n-p "Use external browser? ")
+        (browse-url-default-browser url)
+      (eww-browse-url url)))
+  (setq browse-url-browser-function 'choose-browser)
+  (setf url-cache-directory (expand-file-name "url" var-dir)))
+
+;; eww
+(use-package eww
+  :defer t
+  :commands (eww eww-mode))
