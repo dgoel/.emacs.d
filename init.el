aeb42e6519f56b4ac840f22e31287b62cf50b2da
Dhiraj Goel <dhiraj.goel@gmail.com>
2016-06-26 21:50:25 -0700

    Updates to init.el

--- init.el
+++ init.el
@@ -1,12 +1,15 @@
+;;; init -- entry point for Emacs customization
+;;;
+;;; Code:
+
 ;; Turn off mouse interface early in startup to avoid momentary display
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-;; Benchmark packages
-(add-to-list 'load-path (expand-file-name "benchmark" user-emacs-directory))
-(require 'benchmark-init-loaddefs)
-(benchmark-init/activate)
+;; Enable to track down very deep recursion
+;; (setq max-specpdl-size 5)  ; default is 1000, reduce the backtrace level
+;; (setq debug-on-error t)    ; now you should get a backtrace
 
 ;; Set path to dependencies
 (defconst site-lisp-dir
@@ -17,31 +20,42 @@
   (expand-file-name "var" user-emacs-directory))
 
 ;; Set up load path
-(add-to-list 'load-path user-emacs-directory)
 (add-to-list 'load-path site-lisp-dir)
-(add-to-list 'load-path etc-dir)
 
-;; Keep emacs Custom-settings in separate file
-;; (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-;; (load custom-file)
-
-;; Settings for currently logged in user
-;; (setq user-settings-dir
-;;       (concat user-emacs-directory "users/" user-login-name))
-;; (add-to-list 'load-path user-settings-dir)
+;; Benchmark packages
+(add-to-list 'load-path (expand-file-name "benchmark-init-el" site-lisp-dir))
+(require 'benchmark-init-loaddefs "~/.emacs.d/site-lisp/benchmark-init-el/benchmark-init-loaddefs")
+(benchmark-init/activate)
 
 ;; Add external projects to load path
-(dolist (project (directory-files site-lisp-dir t "\\w+"))
-  (when (file-directory-p project)
-    (add-to-list 'load-path project)))
+;; TODO: this is not needed if load-path is specified in use-package
+;; (dolist (project (directory-files site-lisp-dir t "\\w+"))
+;;   (when (file-directory-p project)
+;;     (add-to-list 'load-path project)))
 
-;; Load libraries first
-(require 'use-package)
+;; use-package
+(defconst use-package-dir
+      (expand-file-name "use-package" site-lisp-dir))
+(add-to-list 'load-path use-package-dir)
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
+;; for benchmark
+(setq use-package-verbose t)
+(setq use-package-minimum-reported-time 0.001)
+
+;; require all packages to be installed
+;; (setq use-package-always-ensure t)
 
 ;; Setup packages
 (defconst package-user-dir
       (expand-file-name "elpa" user-emacs-directory))
 (package-initialize)
+
+(require 's)
+(require 'dash)
 
 ;; Functions (load all files in defuns-dir)
 (setq defuns-dir (expand-file-name "defuns" user-emacs-directory))
@@ -49,8 +63,16 @@
   (when (file-regular-p file)
     (load file)))
 
+;; ;; Load lisp code from lisp dir
+;; (setq lisp-dir
+;;       (expand-file-name "lisp" user-emacs-directory))
+;; (dolist (file (directory-files lisp-dir t "\\w+"))
+;;   (when (file-regular-p file)
+;;     (load file)))
+
 ;; Load files from etc/
-(dolist (file (directory-files etc-dir t "\\w+"))
+(add-to-list 'load-path etc-dir)
+(dolist (file (directory-files etc-dir t "\\.el$"))
   (when (file-regular-p file)
     (load file)))
 
@@ -58,8 +80,7 @@
 ;; Setup extensions
 ; (eval-after-load 'grep '(require 'setup-rgrep))
 
-; (use-package delsel)
-(use-package key-bindings)
+(require 'key-bindings "~/.emacs.d/key-bindings.el")
 
 ;; Emacs server
 (use-package edit-server
