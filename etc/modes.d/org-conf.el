96be86b0ccdb0f49ad9714b584403c84cba44499
Dhiraj Goel <dhiraj.goel@gmail.com>
2016-09-21 22:56:49 -0700

    Updates to org mode confiuration

--- etc/modes.d/org-conf.el
+++ etc/modes.d/org-conf.el
@@ -32,17 +32,8 @@
 ;; Log when a task is done
 (setq org-log-done t)
 
-;; Very popular -- fast state change
+;; Very popular -- fast todo state change
 (setq org-use-fast-todo-selection t)
-
-;; Don't hide blank lines
-; (setq org-cycle-separator-lines 0)
-
-;; Enable auto-fill
-(add-hook 'org-mode-hook 'turn-on-filladapt-mode)
-
-;; highlight current line
-(add-hook 'org-agenda-mode-hook '(lambda () (hl-line-mode 1)))
 
 ;; Line wrapping
 (setq org-startup-truncated nil)
@@ -50,9 +41,63 @@
 ;; Fontify code blocks
 (setq org-src-fontify-natively t)
 
+;; Don't hide blank lines
+;; (setq org-cycle-separator-lines t)
+
+;; Enable auto-fill
+;; http://orgmode.org/manual/Conflicts.html recommends to turn-off filladapt
+;; mode for org-mode due to conflicts
+(add-hook 'org-mode-hook 'turn-on-filladapt-mode)
+
+;; org-todo states
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n!)" "|" "DONE(d!)")
+              (sequence "WAITING(w@/!)" "|" "CANCELLED(c@/!)")
+              (sequence "MEETING" "TASK" "PROJ"))))
+(setq org-todo-keyword-faces
+      '(("TODO" . (:foreground "DarkOrange1" :weight bold))
+        ("NEXT" . (:foreground "light sea green" :weight bold))
+        ("DONE" . (:foreground "royal blue" :weight bold))
+        ("WAITING" . (:foreground "orange red" :weight bold))
+        ("CANCELED" . (:foreground "slate grey" :weight bold))
+        ("PROJ" . (:foreground "magenta" :weight bold))
+        ("MEETING" . (:foreground "magenta" :weight bold))
+        ("TASK" . (:foreground "magenta" :weight bold))))
+
+;; highlight current line
+(add-hook 'org-agenda-mode-hook '(lambda () (hl-line-mode 1)))
+(setq org-agenda-files (file-expand-wildcards "~/org/*.org"))
+(setq org-agenda-custom-commands
+      '(("w" "Weekly Review"
+         ((agenda "" ((org-agenda-ndays 7))) ;; review upcoming deadlines and appointments
+          ;; type "l" in the agenda to review logged items
+          ;;(stuck "") ;; review stuck projects as designated by org-stuck-projects
+          (todo "TODO") ;; review all projects
+          (todo "NEXT")
+          (todo "WAITING"))) ;; review waiting items
+        ;; ...other commands here
+        ))
+
 ;; Capture
 (setq org-default-notes-file
       (concat org-directory "/refile.org"))
+
+;; Example org-capture templates:
+;; http://orgmode.org/manual/Easy-templates.html#Easy-templates
+(setq org-capture-templates
+      (quote (("t" "todo" entry (file "~/org/refile.org")
+               "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("r" "respond" entry (file "~/org/refile.org")
+               "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+              ("n" "note" entry (file "~/org/refile.org")
+               "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("w" "org-protocol" entry (file "~/org/refile.org")
+               "* TODO Review %c\n%U\n" :immediate-finish t)
+              ("m" "Meeting" entry (file "~/org/refile.org")
+               "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
+              ("h" "Habit" entry (file "~/org/refile.org")
+               "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
+
 
 ;; http://endlessparentheses.com/ispell-and-org-mode.html
 ;; Improve spell check in org mode
@@ -81,19 +126,12 @@
    (screen . nil)
    (sh . t)))
 
-;; org-todo keywords
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "|" "DONE(d@!)" "CANCELED(c@)")
-        (sequence "TASK(f)" "|" "DONE(d)" "|" "POSTPONED(p@)")
-        (sequence "MAYBE(m)" "|" "CANCELED(c@)")))
-(setq org-todo-keyword-faces
-      '(("TODO" . (:foreground "DarkOrange1" :weight bold))
-        ("MAYBE" . (:foreground "sea green" :weight bold))
-        ("DONE" . (:foreground "light sea green" :weight bold))
-        ("CANCELED" . (:foreground "slate grey" :weight bold))
-        ("POSTPONED" . (:foreground "dark green" :weight bold))
-        ("TASK" . (:foreground "magenta" :weight bold))))
-
+;; Standard key bindings
+;; TODO should these be mode-specific?
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(global-set-key (kbd "C-c c") 'org-capture)
 
 ;; Latex export for org-article class
 (require 'ob-latex)
