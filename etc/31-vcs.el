aab6898d5d69cd7ae79c2d1e5e6c587b5ee73078
Dhiraj Goel <dhiraj.goel@gmail.com>
2017-04-11 22:47:29 -0700

    Updates to vcs modes

--- etc/31-vcs.el
+++ etc/31-vcs.el
@@ -1,26 +1,46 @@
 (use-package magit
-  :commands magit-status magit-blame
-  :bind ("C-x g" . magit-status)
-  :init
-  (setf magit-last-seen-setup-instructions "1.4.0")
-  (defadvice magit-status (around magit-fullscreen activate)
-    (window-configuration-to-register :magit-fullscreen)
-    ad-do-it
-    (delete-other-windows))
+  :bind
+  ("C-x g s" . magit-status)
+  ("C-x g x" . magit-checkout)
+  ("C-x g c" . magit-commit)
+  ("C-x g e" . magit-ediff-resolve)
+  ("C-x g r" . magit-rebase-interactive)
   :config
-  ;; Ask when staging all files
+  ;; use ivy for completion
+  (require 'ivy)
+  (setq magit-completing-read-function 'ivy-completing-read)
+  ;; confirm when staging all files in one go
   (set-default 'magit-stage-all-confirm t)
-  (setq magit-branch-arguments nil
-        ;; use ido to look for branches
-        magit-completing-read-function 'magit-ido-completing-read
-        ;; don't put "origin-" in front of new branch names by default
-        magit-default-tracking-name-function 'magit-default-tracking-name-branch-only
-        magit-push-always-verify t
-        ;; Get rid of the previous advice to go into fullscreen
-        magit-restore-window-configuration t)
+  (setq
+   magit-branch-arguments nil
+   ;; don't put "origin-" in front of new branch names by default
+   magit-default-tracking-name-function 'magit-default-tracking-name-branch-only
+   ;; double check when pushing upstream
+   magit-push-always-verify t)
+  ;; load extensions based on git repository configuration
   (add-hook 'magit-mode-hook 'magit-load-config-extensions))
 
+(use-package magit-popup)
+(global-git-commit-mode)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ediff                                                                  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(use-package ediff
+  :defer 10
+  :config
+  ;; don't start another frame
+  (setq ediff-window-setup-function 'ediff-setup-windows-plain)
+  ;; put windows side by side
+  (setq ediff-split-window-function (quote split-window-horizontally))
+  (setq-default ediff-highlight-all-diffs 'nil)
+  ;; revert windows on exit - needs winner mode
+  ;; (winner-mode)
+  (add-hook 'ediff-after-quit-hook-internal 'winner-undo)
+  (setq ediff-diff-options "-w"))
 
 
 ;; Go back in history with a touch of a button
