e47b6803ba6903adeb3f6d00e72b9c922408c450
Dhiraj Goel <dhiraj.goel@gmail.com>
2017-04-23 22:07:23 -0700

    Updates to projectile configuration

--- etc/32-project.el
+++ etc/32-project.el
@@ -1,27 +1,23 @@
 ;; Projectile
 (use-package projectile
-  :commands projectile-mode projectile-switch-project
+  :commands projectile-mode projectile-global-mode projectile-switch-project
+  :init (hook-into-modes #'(lambda () (projectile-mode 1))
+                         '(prog-mode-hook
+                           org-mode-hook))
   :bind-keymap ("C-c p" . projectile-command-map)
   :config
   (use-package perspective)
   (use-package persp-projectile :config (persp-mode))
   (projectile-mode)
-  ;; (hook-into-modes #'(lambda () (projectile-mode 1))
-  ;;                  '(prog-mode-hook
-  ;;                    c-mode-common-hook
-  ;;                    python-mode-hook
-  ;;                    gud-mode-hook)))
   (setq projectile-enable-caching t
         projectile-enable-idle-timer t
         projectile-verbose t
         tags-revert-without-query t)
-  ;; if ivy is available
-  (if (featurep 'ivy)
-      (setq projectile-completion-system 'ivy))
 
-  ;; (tags-add-table t)
-  (add-hook 'projectile-idle-timer-hook 'lambda() (message "Idle timer function triggered"))
+  ;; use ivy for completion
+  (setq projectile-completion-system 'ivy)
 
+  ;; configure modeline
   (setq projectile-mode-line
         '(:eval (format " [%s]" (projectile-project-name))))
   (setq projectile-remember-window-configs t)
@@ -32,28 +28,8 @@
   ;; disable remote file exists cache
   (setq projectile-file-exists-remote-cache-expire nil)
   ;; ignore
-  (setq projectile-globally-ignored-files (quote ("TAGS" "COMMIT_EDITMSG")))
+  (setq projectile-globally-ignored-files (quote ("TAGS" "COMMIT_EDITMSG"))))
 
-  (defun rejeep-projectile-completion-fn (prompt choises)
-    "Projectile completion function that only shows file name.
-       If two files have same name, new completion appears to
-       select between them. These include the path relative to
-       the project root."
-    (interactive)
-    (let* ((stripped-choises
-            (-uniq (--map (file-name-nondirectory (directory-file-name it)) choises)))
-           (choise
-            (ido-completing-read prompt stripped-choises))
-           (matching-files
-            (-filter
-             (lambda (file)
-               (equal (file-name-nondirectory (directory-file-name file)) choise))
-             choises)))
-      (if (> (length matching-files) 1)
-          (ido-completing-read prompt matching-files)
-        (car matching-files))))
-  (setq projectile-completion-system 'rejeep-projectile-completion-fn)
-  )
 
 ;; counsel interface to projectile
 
