218d1e980c9f8881a29568c4124f37bad0c0bfb1
Dhiraj Goel <dhiraj.goel@gmail.com>
2019-12-24 14:58:04 +0530

    Update config for projectile

--- etc/32-project.el
+++ etc/32-project.el
@@ -1,13 +1,12 @@
 ;; Projectile
 (use-package projectile
   :commands projectile-mode projectile-global-mode projectile-switch-project
-  :init (hook-into-modes #'(lambda () (projectile-mode 1))
-                         '(prog-mode-hook
-                           org-mode-hook))
+  :hook ((prog-mode org-mode) . projectile-mode)
   :bind-keymap ("C-c p" . projectile-command-map)
   :config
   (use-package perspective)
   (use-package persp-projectile :config (persp-mode))
+  (use-package counsel-projectile :config (counsel-projectile-mode))
   (projectile-mode)
   (setq projectile-enable-caching t
         projectile-enable-idle-timer t
@@ -18,8 +17,7 @@
   (setq projectile-completion-system 'ivy)
 
   ;; configure modeline
-  (setq projectile-mode-line
-        '(:eval (format " [%s]" (projectile-project-name))))
+  (setq projectile-mode-line-prefix " ")
   (setq projectile-remember-window-configs t)
 
   ;; cache file
@@ -29,12 +27,3 @@
   (setq projectile-file-exists-remote-cache-expire nil)
   ;; ignore
   (setq projectile-globally-ignored-files (quote ("TAGS" "COMMIT_EDITMSG"))))
-
-
-;; counsel interface to projectile
-
-(use-package counsel-projectile
-  :disabled t
-  :bind (("C-x c v" . counsel-projectile)
-         ("C-x c p" . counsel-projectile-ag))
-  :config (counsel-projectile-on))
