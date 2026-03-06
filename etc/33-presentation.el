843f03d00f036371da3ee4600d6448628f9cf9b1
Dhiraj Goel <dhiraj.goel@gmail.com>
2026-03-06 06:40:56 -0800

    Disables presentation packages

--- etc/33-presentation.el
+++ etc/33-presentation.el
@@ -1,11 +1,11 @@
 ;; reveal.js for presentations
 (use-package ox-reveal
-  ;; :defer 10
-  :disabled
+  :disabled t
   :config
-  (setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/3.0.0/")
+  (setq org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js")
   (setq org-reveal-mathjax t)
   ;;  (setq org-reveal-toc nil)
-  (setq org-reveal-slide-number nil)
-  (use-package htmlize)
-)
+  (setq org-reveal-slide-number nil))
+
+(use-package htmlize
+  :after ox-reveal)
